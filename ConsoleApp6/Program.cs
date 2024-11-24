d936becbd9841877f2493f6bd4e3daad32e89c0d
natashandrigoz <nata_shandrigoz@mail.ru>
2024-11-24 19:03:42 +0200

    add task6
    Определить статический метод void Divisors (int n), который выводит на экран делители числа n. Используя этот метод, вывести на экран делители чисел:
    а) 11;
    б) 50;
    в) m (m вводится с клавиатуры).

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+	internal class Program
+	{
+		/// <summary>
+		/// Основной метод
+		/// </summary>
+		/// <param name="args">string[]</param>
+		static void Main(string[] args)
+		{
+			// Ввод числа
+			int number = GetInput("Введите целое число: ");
+			// Вывод делителей числа
+			Console.WriteLine($"Делители числа {number}:");
+			Divisors(number);
+			Console.Read();
+		}
+		/// <summary>
+		/// Метод для ввода целого числа
+		/// </summary>
+		/// <param name="message">Введите целое число:</param>
+		/// <returns>целое число</returns>
+		static int GetInput(string message)
+		{
+			Console.Write(message);
+			while (true)
+			{
+				try
+				{
+					return Convert.ToInt32(Console.ReadLine());
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("Неверный ввод. Пожалуйста, введите целое число.");
+				}
+			}
+		}
+		/// <summary>
+		/// Статический метод для вывода делителей числа
+		/// </summary>
+		/// <param name="n">натуральное число</param>
+		static void Divisors(int n)
+		{
+			for (int i = 1; i <= n; i++)
+			{
+				if (n % i == 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
